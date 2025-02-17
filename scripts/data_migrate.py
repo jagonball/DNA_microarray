ce38a64fa057fbc2e26cb97aa232f17928adbed0
Johnathan Lin <jagonlsr@pm.me>
2025-02-17 14:39:13 +0800

    Link with remote.

---
+++ scripts/data_migrate.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pathlib import Path
+
+file = Path("D:/Repositories/25_01_Liver_Cancer/data/Microarray_Huh7_Modified.xlsx")
+output_folder = Path("D:/Repositories/25_01_Liver_Cancer/data")
+output_file = "Microarray_Huh7_normalized.txt"
+
+df = pd.read_excel(file)
+print(df.shape)
+print(df.head())
+
+df.to_csv(output_folder / output_file, sep='\t')
+
