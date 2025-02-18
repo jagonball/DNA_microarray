9cbcd5f3c318ba21db655e3a3151eae08ba0b1e7
Johnathan Lin <jagonball@g-mail.nsysu.edu.tw>
2025-02-18 16:55:45 +0800

    Write result to DataFrame.

--- scripts/ncbi_data.py
+++ scripts/ncbi_data.py
@@ -1,7 +1,9 @@
 import subprocess
 from pathlib import Path
 from time import time, strftime, gmtime, ctime
+import pandas as pd
 from utilities import text_color, create_folder, show_time
+import io
 
 __author__ = "Johnathan Lin <jagonball@g-mail.nsysu.edu.tw>"
 __email__ = "jagonball@g-mail.nsysu.edu.tw"
@@ -14,7 +16,7 @@ def main():
     output_file = Path("D:/Repositories/25_01_Liver_Cancer/analysis/test_result.txt")
 
     dataset_command = ["datasets", "summary", "gene", "symbol"]
-    gene_list = ["WIPI2", "KIAA1549L", "ZG16B"]
+    gene_list = ["WIPI2"]#, "KIAA1549L", "ZG16B"]
     ## Showing fields please refer to: https://www.ncbi.nlm.nih.gov/datasets/docs/v2/command-line-tools/using-dataformat/gene-data-reports/
     fields_command = ["--taxon",
                       "human",
@@ -31,24 +33,39 @@ def main():
                      name-id,orientation,transcript-count,protein-count"]
     command = dataset_command + gene_list + fields_command
 
-    # Run the command and capture the output
-    output = subprocess.run(command,
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-
-    # Print the output
-    print(output.stdout.decode("utf-8"))
+    # ## Run the command and print the output
+    # output = subprocess.run(command,
+    #                         shell=True,
+    #                         stdout=subprocess.PIPE,
+    #                         stderr=subprocess.PIPE)
+    # print(output.stdout.decode("utf-8"))
 
 
-    # Run the command and save the output to file.
-    with open(output_file, "w") as f:
-        subprocess.run(command, shell=True, stdout=f)
+    # ## Run the command and save the output to file.
+    # with open(output_file, "w") as f:
+    #     subprocess.run(command, shell=True, stdout=f)
+
+    
+    ## Run the command and pass to a DataFrame.
+    # Execute the command and capture the output.
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    # Check for errors.
+    if result.returncode != 0:
+        print("Error:", result.stderr)
+    else:
+        # Convert the tsv output to a Pandas DataFrame.
+        df = pd.read_csv(io.StringIO(result.stdout), sep="\t")
+
+        # Display the DataFrame.
+        print(df)
+        # Write to file.
+        df.to_csv(output_file, sep='\t', index=False)
 
 
     time_end = time()
     time_used = time_end - time_start
     show_time(time_used, "Total timme taken")
 
-if __name__=="__main__":
+
+if __name__=="__main__": 
     main()
